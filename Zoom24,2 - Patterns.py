86b68f3075709f0bfdd663f541b6bf44b1cf68b1
DN2025-arch <daneloo2175@gmail.com>
2026-01-30 16:48:41 +0000

    Update Zoom24,2 - Patterns.py

--- Zoom24,2 - Patterns.py
+++ Zoom24,2 - Patterns.py
@@ -54,13 +54,43 @@ for row in range(1,quantatity+1):
 
 print("-----------------")
 
-## 54321
-##  5432
-##   543
-##    54
-##     5
+spaces = ""
+for row in range(1,quantatity+1):
+    for column in range(1,quantatity-row+1):
+        print(f"{quantatity - row}", end="")
+    print()
+    spaces += " "
+    print(f"{spaces}",end="")
+spaces = ""
+print()
 
 print("-----------------")
+
+for row in range(1,quantatity+1):
+    for sp in range(1,quantatity-row+1):
+        print(" ",end="")
+    for column in range(1,row+1):
+        print(f"{row}", end="")
+    print()
+
+print("-----------------")
+
+half = quantatity // 2
+##print(half)
+
+for row in range(1,quantatity+1):
+    if row <= half:
+        for sp in range(1,half-row+1):
+            print(" ",end="")
+        for column in range(1,row+1):
+            print(f"@", end="")
+        print()
+    else:
+        for sp in range(half,row):
+            print(" ",end="")
+        for column in range(quantatity,row,-1):
+            print(f"@", end="")
+        print()
 
 ##    @
 ##   @@
