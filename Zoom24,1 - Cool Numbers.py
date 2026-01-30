e3db2d126c47894ce429cc6bda22a9487f9d75b7
DN2025-arch <daneloo2175@gmail.com>
2026-01-30 16:49:44 +0000

    Update Zoom24,1 - Cool Numbers.py

--- Zoom24,1 - Cool Numbers.py
+++ Zoom24,1 - Cool Numbers.py
@@ -61,4 +61,69 @@ else:
         print("This Number isn't a Tech Number")
 
 
-## Happy Number, Duck Number, Hashtag Number, Perfect Number, Spy Number, 
+# Duck Number, Hashtag Number, Perfect Number, Spy Number
+
+
+
+chosen = str(input("Choose a Number -Duck"))
+
+duck = False
+
+if not chosen[0] == "0":
+    for i in chosen:
+        if i == "0":
+            duck = True
+
+if duck:
+    print("This Number is a Duck Number")
+else:
+    print("This Number isn't a Duck Number")
+
+
+chosen = int(input("Choose a Number -Hash"))
+
+total_dig = 0
+temp = chosen
+while temp > 0:
+    total_dig += 1
+    temp = temp // 10
+power = 10 ** (total_dig-1)
+
+if chosen//power == chosen%10:
+    print("This Number is a Hashtag Number")
+else:
+    print("This Number isn't a Hashtag Number")
+
+
+chosen = int(input("Choose a Number -Perfect"))
+
+total = 0
+
+for i in range(1,chosen):
+    if chosen%i == 0:
+        total += i
+
+if chosen == total:
+    print("This Number is a Perfect Number")
+else:
+    print("This Number isn't a Perfect Number")
+
+
+chosen = int(input("Choose a Number -Spy"))
+
+total = 0
+product = 1
+temp = chosen
+while temp > 0:
+    total += temp%10
+    product *= temp%10
+    temp = temp // 10
+
+if total == product:
+    print(f"This Number is a Spy Number")
+else:
+    print(f"This Number is not a Spy Number")
+
+
+
+
